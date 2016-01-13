c2cc7c54786b0d6f7f9537041462bfeac37da273
TommyCakes <tomhwarhurst@aol.com>
2016-01-13 19:14:16 +0000

    Created Comment Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113183908) do
+ActiveRecord::Schema.define(version: 20160113191126) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
